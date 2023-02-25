490052932377ee55ab753f77035a72bb41c23dd9
Maksim <dvmax1984@mail.ru>
2023-02-25 21:01:00 +0700

    Добавил код программы в HW01 Ex004

---
+++ Homework/Homework_01/Ex004/Program.cs
@@ -0,0 +1,28 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
+
+        Console.Write("Введите конечное число N = ");
+        string inputN = Console.ReadLine();
+
+        int n, i = 1;
+
+        int.TryParse(inputN, out n);
+
+        Console.WriteLine($"Четные числа");
+
+        while (i <= n)
+        {
+            if (i%2 == 0)
+            {
+                Console.WriteLine(i);            
+            }
+
+            i++;
+        }
+        
+        
+    }
+}
