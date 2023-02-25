4f27d67e00d158551c701cbd05d36a57ff0334d7
Maksim <dvmax1984@mail.ru>
2023-02-25 15:13:01 +0700

    Добавил диаграмму в HW01, Ex001

--- Homework/Homework_01/Ex001/Program.cs
+++ Homework/Homework_01/Ex001/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача №2, нахождение максимального числа из двух введенных");
+Console.Write("Введите число А = ");
+int A = Console.ReadLine();
+
+Console.Write("Введите число B = ");
+int B = Console.ReadLine();
+
+if (A > B)
