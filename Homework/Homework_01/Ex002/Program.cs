ebdbdff02ec1a5ad5ede61a69bd21f745e748427
Maksim <dvmax1984@mail.ru>
2023-02-25 20:03:20 +0700

    Добавил код программы в HW01 Ex002

--- Homework/Homework_01/Ex002/Program.cs
+++ Homework/Homework_01/Ex002/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+
+        Console.Write("Введите число А = ");
+        string inputA = Console.ReadLine();
+
+        Console.Write("Введите число B = ");
+        string inputB = Console.ReadLine();
+
+        Console.Write("Введите число C = ");
+        string inputB = Console.ReadLine();
+
+        int a, b, c;
+
+        int.TryParse(inputA, out a);
+        int.TryParse(inputB, out b);
+        int.TryParse(inputC, out c);
+
+        // Решим задачу через метод
+
+int max = max(a, b, c);
+Console.WriteLine(max);
+
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
