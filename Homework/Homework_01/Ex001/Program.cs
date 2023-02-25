d688ee4edd112a4487ab9933bd00f4e88776ed1d
Maksim <dvmax1984@mail.ru>
2023-02-25 15:54:46 +0700

    Добавил код программы в HW01, Ex001

--- Homework/Homework_01/Ex001/Program.cs
+++ Homework/Homework_01/Ex001/Program.cs
@@ -1,8 +1,20 @@
 ﻿Console.WriteLine("Задача №2, нахождение максимального числа из двух введенных");
 Console.Write("Введите число А = ");
-int A = Console.ReadLine();
+string inputA = Console.ReadLine();
 
 Console.Write("Введите число B = ");
-int B = Console.ReadLine();
+string inputB = Console.ReadLine();
+
+int A, B;
+
+Int32.TryParse(inputA, out A);
+Int32.TryParse(inputB, out B);
 
 if (A > B)
+{
+    Console.WriteLine("Число A > B: " + Convert.ToString(A));
+}
+else
+{
+    Console.WriteLine("Число B > A:" + Convert.ToString(B));
+}
