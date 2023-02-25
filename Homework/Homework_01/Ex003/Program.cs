07e084a913b11b5cf666d5cde0b704c8bd38830b
Maksim <dvmax1984@mail.ru>
2023-02-25 20:41:18 +0700

    Добавил код программы в HW01 Ex003

--- Homework/Homework_01/Ex003/Program.cs
+++ Homework/Homework_01/Ex003/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).");
+
+        Console.Write("Введите число А = ");
+        string inputA = Console.ReadLine();
+
+        int a;
+
+        int.TryParse(inputA, out a);
+
+        if (a%2 == 0)
+        {
+            Console.WriteLine($"Число {a} является четным");
+        }
+        else
+        {
+            Console.WriteLine($"Число {a} является нечетным");
+        }
+
+    }
+}
