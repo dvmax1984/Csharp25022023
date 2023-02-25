557670deb884237b2d90040733bfc65e605f8e72
Maksim <dvmax1984@mail.ru>
2023-02-25 20:18:41 +0700

    Обновил код программы в HW01 Ex002

--- Homework/Homework_01/Ex002/Program.cs
+++ Homework/Homework_01/Ex002/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
 
         Console.Write("Введите число А = ");
         string inputA = Console.ReadLine();
@@ -7,7 +11,7 @@
         string inputB = Console.ReadLine();
 
         Console.Write("Введите число C = ");
-        string inputB = Console.ReadLine();
+        string inputC = Console.ReadLine();
 
         int a, b, c;
 
@@ -17,14 +21,16 @@
 
         // Решим задачу через метод
 
-int max = max(a, b, c);
-Console.WriteLine(max);
+        int max = Max(a, b, c);
+        Console.WriteLine("Наибольшее число " + max);
+       
+        int Max(int arg1, int arg2, int arg3)
+        {
+            int result = arg1;
+            if (arg2 > result) result = arg2;
+            if (arg3 > result) result = arg3;
+            return result;
+        }
 
-
-int Max(int arg1, int arg2, int arg3)
-{
-    int result = arg1;
-    if (arg2 > result) result = arg2;
-    if (arg3 > result) result = arg3;
-    return result;
+    }
 }
