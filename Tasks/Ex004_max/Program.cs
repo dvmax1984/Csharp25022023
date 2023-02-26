9b6360d6badfeb156422cd778236d0015da29e32
Maksim <dvmax1984@mail.ru>
2023-02-26 21:46:37 +0700

    Добавил ветку TSKEX004

---
+++ Tasks/Ex004_max/Program.cs
@@ -0,0 +1,22 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задача 4. Найти максимальное из трех чисел");
+
+        int[] array = { 2, 4, 1 };
+
+        int max = array[0];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }            
+        }
+
+        Console.WriteLine("Максимальное число "+ max);
+
+    }
+}
