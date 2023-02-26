08c0bae2213b7179f8cf536e2e8ab038f0f60987
Maksim <dvmax1984@mail.ru>
2023-02-26 18:16:03 +0700

    Добавил код в  TSKEX002

--- Tasks/Ex002/Program.cs
+++ Tasks/Ex002/Program.cs
@@ -1,3 +1,22 @@
 ﻿Console.WriteLine("Задача 2. Даны два числа. Показать большее и меньшее число");
 
+Console.Write("Введите число А = ");
+int a = ReadInt(Console.ReadLine());
 
+Console.Write("Введите число B = ");
+int b = ReadInt(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Число А = {{0}} > числа B = {{1}}", a, b);
+}
+else
+{
+    Console.WriteLine($"Число А = {{0}} < числа B = {{1}}", a, b);
+}
+
+int ReadInt(string message)
+{
+    int res = Int32.Parse(message);
+    return res;
+}
