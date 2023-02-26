706ff5f3d904b79589fc9f8fa95eafc8dd3e1ab2
Maksim <dvmax1984@mail.ru>
2023-02-26 11:25:35 +0700

    Добавил код в TSK Ex000

--- Tasks/Ex000/Program.cs
+++ Tasks/Ex000/Program.cs
@@ -1,2 +1,10 @@
-﻿Console.WriteLine("0. Вывести квадрат числа");
+﻿Console.WriteLine("Задача № 0. Вывести квадрат числа");
 
+Console.Write("Введите число для возведения его в квадрат (степень 2): ");
+        //string inputA = Console.ReadLine();
+        //int A;
+        //int.TryParse(inputA, out A);
+
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("{0} в квадрате = {1} ", x, Math.Pow(x, 2));
