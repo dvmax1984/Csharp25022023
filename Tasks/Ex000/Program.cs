51199c561a2e232869b3a65a9e2699e53cf0ec01
Maksim <dvmax1984@mail.ru>
2023-02-26 11:26:58 +0700

    Обновил код в TSK Ex000

--- Tasks/Ex000/Program.cs
+++ Tasks/Ex000/Program.cs
@@ -1,10 +1,16 @@
-﻿Console.WriteLine("Задача № 0. Вывести квадрат числа");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задача № 0. Вывести квадрат числа");
 
-Console.Write("Введите число для возведения его в квадрат (степень 2): ");
+        Console.Write("Введите число для возведения его в квадрат (степень 2): ");
         //string inputA = Console.ReadLine();
         //int A;
         //int.TryParse(inputA, out A);
 
         double x = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("{0} в квадрате = {1} ", x, Math.Pow(x, 2));
+        Console.WriteLine($"{{0}} в квадрате = {{1}} ", x, Math.Pow(x, 2));
+    }
+}
