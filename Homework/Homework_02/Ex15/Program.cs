df8628ffcc0f6458d03c0efcfa887e39295726fd
Maksim <dvmax1984@mail.ru>
2023-02-27 23:16:42 +0700

    Добавил код в пространство для HW02EX15

--- Homework/Homework_02/Ex15/Program.cs
+++ Homework/Homework_02/Ex15/Program.cs
@@ -1,1 +1,41 @@
 ﻿Console.WriteLine("**Задача 15:** Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+
+int ReadNumber(string text)
+{
+    Console.Write(text);
+    int result;
+    while(!int.TryParse(Console.ReadLine(), out result));
+    return result;
+}
+
+bool CheckDayWeek(int d)
+{
+    return (d >= 1 && d <= 7);
+}
+
+bool CheckDayWeekEnd(int d)
+{
+    return (d == 6 || d == 7);
+}
+
+string GetResult(bool dNum, bool dayWeekEnd)
+{
+    if (dNum && dayWeekEnd) 
+    {
+        return "Да, является";
+    }
+    else if (dNum && !dayWeekEnd)
+    {
+        return "Нет, не явлется выходным днем";
+    }
+    else
+    {
+        return "Допускается ввод только чисел от 1 до 7";
+    }
+}
+
+int dNum = ReadNumber("Введите номер дня недели: ");
+bool flagDayWeek = CheckDayWeek(dNum);
+bool flagDayWeekEnd = CheckDayWeekEnd(dNum);
+string output = GetResult(flagDayWeek, flagDayWeekEnd);
+Console.WriteLine(output);
