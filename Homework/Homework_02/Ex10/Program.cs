6b53ca8a71e98925df8dafc26ea09924d4198117
Maksim <dvmax1984@mail.ru>
2023-02-27 21:37:49 +0700

    Добавил код программы HW2 Ex10

--- Homework/Homework_02/Ex10/Program.cs
+++ Homework/Homework_02/Ex10/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("**Задача 10:** Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+
+string ReadText(string text)
+{
+    Console.Write(text);
+    return Console.ReadLine();
+}
+
+bool Check(string s)
+{
+    return s.Length == 3;
+}
+
+string GetResult(string s, bool check)
+{
+    if (check) 
+    {
+        return "Второй символ: " + s.Substring(1, 1);
+    }
+    else
+    {
+        return "Число должно быть трехзначным!";
+    }
+}
+
+string promt = ReadText("Введите трехзначное число: ");
+bool flag = Check(promt);
+string output = GetResult(promt, flag);
+Console.WriteLine(output);
