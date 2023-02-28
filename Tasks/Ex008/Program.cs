8723046cf3e785a9dfba95a5d6fec6ac1edd3c5d
Maksim <dvmax1984@mail.ru>
2023-02-28 23:36:36 +0700

    Подготовил пространство для Ex008 в разделе TASK

---
+++ Tasks/Ex008/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("8. Показать четные числа от 1 до N");
+
+string PrintNumber(int start, int end)
+{
+    string s = string.Empty;
+    string sep = string.Empty;
+
+    for (int i = start; i <= end; i++)
+    {
+        if (i != end) {sep = ", ";} else {sep = string.Empty;}
+
+        s+= Convert.ToString(i) + sep;
+    }
+    return s;
+}
+
+string s = PrintNumber(-5, 5);
+Console.WriteLine(s);
