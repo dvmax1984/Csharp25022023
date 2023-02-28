eff8e1e3c056ca6a1a0370331194d449a6384b81
Maksim <dvmax1984@mail.ru>
2023-02-28 22:39:20 +0700

    Решил задачу TASK Ex006

--- Tasks/Ex006/Program.cs
+++ Tasks/Ex006/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача 6. Выяснить является ли число чётным");
+
+int ReadNumber(string text)
+{
+    Console.Write(text);
+    int result;
+    while(!int.TryParse(Console.ReadLine(), out result));
+    return result;
+}
+
+
+string GetResult(int n)
+{
+    if (n % 2 == 0) 
+    {
+        return "Число четное";
+    }
+    else
+    {
+        return "Число не четное";
+    }
+}
+
+int n = ReadNumber("Введите целое число: ");
+string output = GetResult(n);
+Console.WriteLine(output);
