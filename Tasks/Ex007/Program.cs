176c9d92c077e84804ec7545a4811a21de3d9181
Maksim <dvmax1984@mail.ru>
2023-02-28 23:28:26 +0700

    Добавил код в Ex007 TASK

--- Tasks/Ex007/Program.cs
+++ Tasks/Ex007/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("7. Показать числа от -N до N");
+
+string PrintNumber(int start, int end)
+{
+    string s = string.Empty;
+    string sep = string.Empty;
+
+    for (int i = start; i <= end; i++)
+    {
+        if (i != end) {sep = ", ";} else {sep = string.Empty;}
+
+        s+= Convert.ToString(i) + sep;
+    }
+    return s;
+}
+
+string s = PrintNumber(-5, 5);
+Console.WriteLine(s);
