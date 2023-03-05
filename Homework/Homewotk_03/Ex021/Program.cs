f668cb66a36823ef52b659ca94d6642102a314f2
Maksim <dvmax1984@mail.ru>
2023-03-05 09:25:23 +0700

    Решил задачу HW03Ex021

--- Homework/Homewotk_03/Ex021/Program.cs
+++ Homework/Homewotk_03/Ex021/Program.cs
@@ -2,19 +2,30 @@
 Console.WriteLine("----------------------------------------");
 Console.WriteLine("Принимаем координаты точки B(x1, y1, z1)");
 
-Console.Write("Введите x1");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите y1");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите x1");
-int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x1: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y1: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z1: ");
+double z1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("----------------------------------------");
 Console.WriteLine("Принимаем координаты точки B(x2, y2, z2)");
 
-Console.Write("Введите x2");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите y2");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите x2");
-int z2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x2: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y2: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x2: ");
+double z2 = Convert.ToInt32(Console.ReadLine());
 
+// L = SQR ((x2 - x1)^2 + (y2 + y1)^2 - (z2 + z1)^2)
+
+double x = Math.Pow(x2 - x1, 2);
+double y = Math.Pow(y2 - y1, 2);
+double z = Math.Pow(z2 - z1, 2);
+
+double l = Math.Sqrt(x + y + z);
+
+l = Math.Round(l, 2);
+
+Console.WriteLine("Расстояние в просмтрансве между точками A и B = " + l);
