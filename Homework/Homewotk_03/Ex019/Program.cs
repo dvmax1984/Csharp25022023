575616e74f5b9201b4ebf2fa3382efb70f5c1a8f
Maksim <dvmax1984@mail.ru>
2023-03-05 18:48:12 +0700

    Решил задачу HW03Ex019 методами и классом

--- Homework/Homewotk_03/Ex019/Program.cs
+++ Homework/Homewotk_03/Ex019/Program.cs
@@ -1,36 +1,6 @@
-﻿Console.WriteLine("Задача № 19. Напишите метод(-ы), который принимает на вход пятизначное число и проверяет, является ли оно палиндромом.!");
+﻿using static Library;
 
-int GetNumber(string s)
-{
-    Console.Write(s);
-
-    int result;
-    while (!int.TryParse(Console.ReadLine(), out result)) ;
-    return result;
-}
-
-bool CheckLenghtNumber(int n)
-{
-    if (n < 10000 || n > 99999)
-    {
-        Console.WriteLine("Число должно быть пятизначным!");
-        return false;
-    }
-    return true;
-
-}
-
-string GetResult(int n)
-{
-    if (n % 10 == n / 10000)
-    {
-        if ((n / 1000) % 10 == (n / 10) % 10)
-        {
-            return ("Число является палиндромом");
-        }
-    }
-    return ("Число не является палиндромом");
-}
+Console.WriteLine("Задача № 19. Напишите метод(-ы), который принимает на вход пятизначное число и проверяет, является ли оно палиндромом.!");
 
 int num = GetNumber("Введите пятизначное число n: ");
 bool lenghNum = CheckLenghtNumber(num);
