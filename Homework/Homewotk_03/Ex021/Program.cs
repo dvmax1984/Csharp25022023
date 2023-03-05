84ab228af537ae5275e51e7abf990f5177d3a978
Maksim <dvmax1984@mail.ru>
2023-03-05 09:13:41 +0700

    Создал блок-схему

--- Homework/Homewotk_03/Ex021/Program.cs
+++ Homework/Homewotk_03/Ex021/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача 21 Напишите метод(-ы), который принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.");
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Принимаем координаты точки B(x1, y1, z1)");
+
+Console.Write("Введите x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Принимаем координаты точки B(x2, y2, z2)");
+
+Console.Write("Введите x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
