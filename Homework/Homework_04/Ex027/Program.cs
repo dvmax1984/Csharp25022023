d2845e6ec44b68e636f4614fb9a76a11209545d0
dvmax1984 <dvmax1984@mail.ru>
2023-03-07 00:43:13 +0700

    Решил задачу 27 методами и классами дописал код

--- Homework/Homework_04/Ex027/Program.cs
+++ Homework/Homework_04/Ex027/Program.cs
@@ -4,4 +4,4 @@
 Console.WriteLine("Задача 27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
 
 int num = GetNumber("Введите число: ");
-Console.WriteLine(GetResult(num));
+Console.WriteLine($"В числе {num} cумма чисел = {GetResult(num)}");
