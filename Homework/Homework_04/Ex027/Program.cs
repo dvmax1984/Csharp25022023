b276333eda56ec98a73044e87f6af7c5a2e34422
dvmax1984 <dvmax1984@mail.ru>
2023-03-07 00:23:38 +0700

    Решил задачу 27 методами и классами

--- Homework/Homework_04/Ex027/Program.cs
+++ Homework/Homework_04/Ex027/Program.cs
@@ -1,23 +1,7 @@
-﻿Console.WriteLine("Задача 27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
+﻿using static Library;
 
-int GetNumber(string s)
-{
-    Console.Write(s);
-    int n;
-    while (!int.TryParse(Console.ReadLine(), out n)) ;
-    return n;
-}
 
-int GetResult(int n)
-{
-    int res = 0;
-    while (n > 0)
-    {
-        res = res + n % 10;
-        n = n / 10;
-    }
-    return res;
-}
+Console.WriteLine("Задача 27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
 
 int num = GetNumber("Введите число: ");
 Console.WriteLine(GetResult(num));
