34c3eada2d9e0699821c33dce175820545325b24
Maksim <dvmax1984@mail.ru>
2023-03-06 19:57:55 +0700

    Решил задачу 25 методами

--- Homework/Homework_04/Ex025/Program.cs
+++ Homework/Homework_04/Ex025/Program.cs
@@ -1,3 +1,19 @@
 ﻿Console.WriteLine("Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
 
+double GetNumber(string s)
+{
+    Console.Write(s);
+    double result;
+    while(!double.TryParse(Console.ReadLine(), out result));
+    return result;
+}
 
+double GetResult(double a, double b)
+{
+    return Math.Pow(a, b);
+}
+
+double a = GetNumber("Введите число A возводимое в степерь: ");
+double b = GetNumber("Введите число B степерь числа A: ");
+
+Console.WriteLine(GetResult(a, b));
