a991e8bfe283aa4f4f41964f580a47366c4f2a3c
Maksim <dvmax1984@mail.ru>
2023-03-06 19:37:10 +0700

    Разработал блок-схему

--- Homework/Homework_04/Ex025/Program.cs
+++ Homework/Homework_04/Ex025/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
+
+
