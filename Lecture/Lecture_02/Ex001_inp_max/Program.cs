a6a418efce3bc3f39e1c929705bfad5a95729a24
dvmax1984 <dvmax1984@mail.ru>
2023-03-08 13:01:20 +0700

    Создал блок схему задача 29 ДЗ 4

--- Lecture/Lecture_02/Ex001_inp_max/Program.cs
+++ Lecture/Lecture_02/Ex001_inp_max/Program.cs
@@ -7,16 +7,16 @@
     return result;
 }
 
-int GetData()
+int GetData(string s)
 {
-    Console.Write("Введите значение: ");
+    Console.Write(s); //"Введите значение: ");
     return Convert.ToInt32(Console.ReadLine());
 }
 
 int max = Max(
-   Max(GetData(), GetData(), GetData()),
-   Max(GetData(), GetData(), GetData()),
-   Max(GetData(), GetData(), GetData())
+   Max(GetData("Введите значение 1: "), GetData("Введите значение 2: "), GetData("Введите значение 3: ")),
+   Max(GetData("Введите значение 4: "), GetData("Введите значение 5: "), GetData("Введите значение 6: ")),
+   Max(GetData("Введите значение 7: "), GetData("Введите значение 8: "), GetData("Введите значение 9: "))
 );
 
 Console.WriteLine(max);
