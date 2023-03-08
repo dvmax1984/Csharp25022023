d028656b55c4e3c6bb1ceca3db4af3dcdc260dc9
dvmax1984 <dvmax1984@mail.ru>
2023-03-08 11:58:21 +0700

    Добавил пример ввода из УРОКА 2 находение максимума

---
+++ Lecture/Lecture_02/Program.cs
@@ -0,0 +1,22 @@
+﻿int Max(int args1, int args2, int args3)
+{
+    int result = 0;
+    if (args1 > result) result = args1;
+    if (args2 > result) result = args2;
+    if (args3 > result) result = args3;
+    return result;
+}
+
+int GetData()
+{
+    Console.Write("Введите значение: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int max = Max(
+   Max(GetData(), GetData(), GetData()),
+   Max(GetData(), GetData(), GetData()),
+   Max(GetData(), GetData(), GetData())
+);
+
+Console.WriteLine(max);
