c5ff2b1e52685285a2f10a00ad96064702a0a4c0
Maxim Dvortcov <dvmax@outlook.com>
2023-03-09 09:59:21 +0700

    Написал методы в классе

--- Homework/Homework_04/Ex029/Program.cs
+++ Homework/Homework_04/Ex029/Program.cs
@@ -1,4 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.");
  
 
