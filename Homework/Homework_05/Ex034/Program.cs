865684bfea3bf9b08d4de01fbafe6ecf6458e074
dvmax1984 <dvmax1984@mail.ru>
2023-03-09 21:41:19 +0700

    решил задачу 34 - написал метод вызывающий методы из класса для задачи 34 дз 5

--- Homework/Homework_05/Ex034/Program.cs
+++ Homework/Homework_05/Ex034/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Methods;
+
+Console.WriteLine("Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.");
+
+int arrlenght = new Random().Next(9, 19);
+int[] array = new int[arrlenght];
+
+FillArray(array);
+
+Console.WriteLine("В массиве четных числел: " + GetResult(array));
--- Homework/Homework_05/Ex034/methods.cs
+++ Homework/Homework_05/Ex034/methods.cs
@@ -2,15 +2,30 @@ public static class Methods
 {
     public static void FillArray(int[] arr)
     {
+        Console.WriteLine($"Массив из целых положительных чисел 1 до 999 произвольной длинной от 9 до 19, сейчас длинна = {{0}}", arr.Length);
+
+        Console.Write("[ ");
+
         for (int i = 0; i < arr.Length; i++)
         {
             arr[i] = new Random().Next(0, 999);
+            
+            if (i != arr.Length - 1)
+            {
+                Console.Write(arr[i] + ", ");
+            }
+            else
+            {
+                Console.Write(arr[i] + " ]");
+            }
+
         }
+        Console.WriteLine();
     }
 
     public static int GetResult(int[] arr)
     {
-        int res;
+        int res = 0;
         for (int j = 0; j < arr.Length; j++)
         {
             if (arr[j] % 2 == 0)
