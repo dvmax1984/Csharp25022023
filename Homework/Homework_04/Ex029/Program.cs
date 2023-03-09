e6f21464cc78a279339cfaf25c598071f70d16d1
Maxim Dvortcov <dvmax@outlook.com>
2023-03-09 10:04:22 +0700

    Написал вызов методов из класса

--- Homework/Homework_04/Ex029/Program.cs
+++ Homework/Homework_04/Ex029/Program.cs
@@ -1,3 +1,11 @@
-﻿Console.WriteLine("Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.");
- 
+﻿using static Library;
 
+Console.WriteLine("Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.");
+
+int len = GetNumber("Укажите длинну массива");
+
+int[] array = new int[len];
+
+FillArray(array);
+
+Console.Write(GetResult(array));
