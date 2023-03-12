d9a2f4fc8caf7db826ca0e199c46c56e0f564733
dvmax1984 <dvmax1984@mail.ru>
2023-03-12 12:44:47 +0700

    Написал код поворота матрицы на 90 град по часовой стрелке

---
+++ Lecture/Lecture_04/Ex000_Matrix/Library.cs
@@ -0,0 +1,41 @@
+public static class Library
+{
+    public static void PrintArray(int[,] arr)
+    {
+        for (int row = 0; row < arr.GetLength(0); row++)
+        {
+            for (int col = 0; col < arr.GetLength(1); col++)
+            {
+                Console.Write(arr[row, col]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static int[,] ArrRotate90(int[,] arr)
+    {
+        int rowEnd = arr.GetLength(0)-1;
+        int colEnd = arr.GetLength(1)-1;
+        int[,] arrRot2 = new int[arr.GetLength(0), arr.GetLength(1)];
+
+        for (int row = 0; row <= arr.GetLength(0)-1; row++)
+        {
+            rowEnd = 3; //arr.GetLength(0);
+            for (int col = 0;  col <= colEnd; col++)
+            {
+                arrRot2[row, col] = arr[rowEnd, row];
+                            
+                            //Console.WriteLine($"arrRot2 = {arrRot2[row, col]} << arr = {arr[row, col]}");
+                
+                //Console.WriteLine($"rowEnd = {rowEnd}");
+                rowEnd = rowEnd - 1;
+                //Console.WriteLine($"rowEnd = {rowEnd}");
+                //Console.WriteLine($"col = {col}");
+                //Console.WriteLine($"row = {row}");
+            }
+
+            //arrRot2[0, 2] = arr[0, 0];
+        }
+        return arrRot2;
+    }
+}
---
+++ Lecture/Lecture_04/Ex000_Matrix/Program.cs
@@ -0,0 +1,13 @@
+﻿using static Library;
+
+int[,] pic = new int[,]
+{ {1,   1,      0,      0       },   /* row 0      7, 4, 1   */
+  {0,   0,      1,      1       },   /* row 1      8, 5, 2   */
+  {0,   0,      0,      0      },
+  {0,   0,      0,      0      } }; /* row 2      9, 6, 3   */
+
+PrintArray(pic);
+
+int[,] arrRot = ArrRotate90(pic);
+Console.WriteLine();
+PrintArray(arrRot);
