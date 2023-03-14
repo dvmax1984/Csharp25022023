82168f658497f1fdd4eaf367d99f5ee7b1039327
Maksim Dvortsov <55104764+dvmax1984@users.noreply.github.com>
2023-03-14 04:19:14 +0000

    ДЗ 6 задача 41 - почти почти работает

--- Homework/Homework_06/Ex041/Library.cs
+++ Homework/Homework_06/Ex041/Library.cs
@@ -1,16 +1,17 @@
 public static class Library
 {
-    public static int GetNumber(string s, int[] arr, int n)
+    public static int GetNumber(string s, int[] arr, int n, int count)
     {
         Console.Write(s);
 
-        if (n == arr.Length) return 0;
+        if (n == arr.Length) return count+100;
 
         int result;
         while (!int.TryParse(Console.ReadLine(), out result)) ;
         arr[n] = result;
+        if (result > 0) {count += 1;}
         n += 1;
-        GetNumber($"Шаг {n} из {arr.Length} - {arr[n - 1]} ", arr, n);
-        return 0;
+        count += GetNumber($"Шаг {n} из {arr.Length} - {arr[n - 1]} - count {count} ", arr, n, count);
+        return count+1000;
     }
 }
--- Homework/Homework_06/Ex041/Program.cs
+++ Homework/Homework_06/Ex041/Program.cs
@@ -1,9 +1,14 @@
-﻿using static Library;
+using static Library;
 
 Console.WriteLine("Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
 
 int[] arrNumber = new int[9];
 
-int n = GetNumber("Введите несколько чисел M:> ", arrNumber, 0);
+int count = GetNumber("Введите несколько чисел M:> ", arrNumber, 0, 0);
 
-Console.WriteLine(n);
+Console.WriteLine($"Count = {count} ");
+
+for (int i = 0; i < arrNumber.Length; i++)
+  {
+    Console.WriteLine(arrNumber[i]);
+  }
