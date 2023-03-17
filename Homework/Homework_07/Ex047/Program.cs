fbad122924e57e0f28b224a3231600083d748005
Maksim Dvortsov <55104764+dvmax1984@users.noreply.github.com>
2023-03-17 05:00:04 +0000

    Правка кода задачи 47 ДЗ 7

--- Homework/Homework_07/Ex047/Program.cs
+++ Homework/Homework_07/Ex047/Program.cs
@@ -3,7 +3,7 @@ using static classFillArray;
 using static classPrintArray;
 using static classGetNumber;
 
-Console.WriteLine("Задать двумерный массив размером m×n, заполненный случайными вещественными числами (-99,9 до 99,9)");
+Console.WriteLine("Задать двумерный массив размером m×n, заполненный случайными вещественными числами (от -99,9 до 99,9)");
 
 int m = GetNumber("Задайте размер массива, кол-во Строк m : ");
 int n = GetNumber("Задайте размер массива, кол-во Колонок n : ");
@@ -14,4 +14,3 @@ array = FillArray(array);
 
 PrintArray(array);
 
-Console.WriteLine();
