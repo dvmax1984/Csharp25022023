9a77ed7faad6c4f16d1578226f6b6bb58f1de31d
Maksim Dvortsov <55104764+dvmax1984@users.noreply.github.com>
2023-03-17 04:51:49 +0000

    Решил задачу 74 ДЗ 7

---
+++ Homework/Homework_07/Ex047/classPrintArray.cs
@@ -0,0 +1,15 @@
+
+public static class classPrintArray
+{
+    public static void PrintArray(double[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]}   ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
