ec1142b91e990f3400f2914f746fb177731ab83d
Maksim <dvmax1984@mail.ru>
2023-03-20 02:05:09 +0700

    Решил задачу - Треугольник Паскаля

---
+++ Seminar/Seminar_07/Ex001_Triang_Pascla_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using static clPascalTriangle;
+
+Console.WriteLine("Треугольник Паскаля, до 14 строк, 15 уже хуже рисует");
+
+int heigth = GetNumber("Введите высоту треугольника, строк: ");
+
+int[,] arr = new int[heigth, heigth*3];
+
+int[,] arrPasc = FillArray(arr, 3, heigth);
+
+PrintArray(arrPasc);
+
+Console.WriteLine();
+
+int[,] arrMove = MoveArray(arrPasc, heigth);
+
+PrintArray(arrMove);
---
+++ Seminar/Seminar_07/Ex001_Triang_Pascla_2/classPascalTriangle.cs
@@ -0,0 +1,79 @@
+public static class clPascalTriangle
+{
+    /// <summary>
+    /// Метод FillArray() заполняет массив по формуле треугольника Паскаля
+    /// </summary>
+    /// <param name="arr">Аргумент FillArray() сообщает массив для заполнения</param>
+    /// <param name="sc">Аргумент FillArray() сообщает шаг</param>
+    /// <param name="height">Аргумент FillArray() сообщает высоту треугольника в строках</param>
+    public static int[,] FillArray(int[,] arr, int sc, int height)
+    {
+        arr[0, 0] = 1; arr[1, 0] = 1; arr[1, sc] = 1;
+
+        for (int i = 2; i < arr.GetLength(0); i++)              // row
+        {
+            for (int j = 0; j < arr.GetLength(1); j += sc)      // col
+            {
+                if (j == 0) { arr[i, j] = 1; }
+                else { arr[i, j] = arr[i - 1, j - sc] + arr[i - 1, j]; }
+            }
+        }
+
+        return arr;
+    }
+
+    /// <summary>
+    /// Метод MoveArray() центрирует треугольник Паскаля в самом массиве.
+    /// Начало центрирования с основания треугольника
+    /// Массив сдвигается в право, пример: 11>>0 , 10>>11 , 9>>10 , 8>>9 итд
+    /// </summary>
+    public static int[,] MoveArray(int[,] arr, int height)
+    {
+        int n = arr.GetLength(1);                        // ширина массива, колонки
+        int mR = 1;                                      // сдвиг, начальный центр
+
+        for (int row = arr.GetLength(0)-1; row >= 0; row--)
+        {  
+            for (int i = 0; i < mR; i++)                 // сдвиг на k
+            {
+                int LastVal = arr[row, n - 1];           //запомнить последнее значение последней колонки
+
+                for (int col = n - 1; col > 0; col--)
+                {
+                    arr[row, col] = arr[row, col - 1];
+                }
+                arr[row, 0] = LastVal;
+            }
+            
+            mR += 3; //инкремент mR по уменьшению ряда, чем меньше ряд, тем больше требуется смещение
+        }
+
+        return arr;
+    }
+
+
+    public static void PrintArray(int[,] a)
+    {
+        for (int m = 0; m < a.GetLength(0); m++)
+        {
+            for (int n = 0; n < a.GetLength(1); n++)
+            {
+                if (a[m, n] >= 1 && a[m, n] <= 9)       Console.Write($"   {a[m, n]}");
+                else if (a[m, n] >= 10 && a[m, n] <= 99)     Console.Write($"  {a[m, n]}");
+                else if (a[m, n] >= 100 && a[m, n] <= 999)   Console.Write($" {a[m, n]}");
+                else if (a[m, n] >= 1000 && a[m, n] <= 9999) Console.Write($"{a[m, n]}");
+                else if (a[m, n] == 0) Console.Write($" ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static int GetNumber(string s)
+    {
+        Console.Write(s);
+
+        int n;
+        while (!int.TryParse(Console.ReadLine(), out n)) ;
+        return n;
+    }
+}
