93ba815bdb9b6eb31c51eccd130ad5014249ed13
dvmax1984 <dvmax1984@mail.ru>
2023-03-21 21:48:29 +0700

    Добавил задачу по заполнению массива рекурсией из Лекции 7

---
+++ Lecture/Lecture_07/Ex000_Fill_Rec/Program.cs
@@ -0,0 +1,41 @@
+﻿int[,] pic = new int[10, 10];
+
+for (int i = 0; i < 10; i++)
+{
+    pic[0, i] = 1;
+    pic[i, 0] = 1;
+    pic[i, 9] = 1;
+    pic[9, i] = 1;
+}
+
+PrintImage(pic);
+//FillImage(1, 1);
+Console.WriteLine();
+//PrintImage(pic);
+
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            //Console.Write($"{image[i,j]} ");
+            if (image[i, j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
