afe4ee1a5769d5863a6b51911f1f18a77da6001e
Maksim <dvmax1984@mail.ru>
2023-03-22 23:20:16 +0700

    Решил задачу 62 ДЗ 8

---
+++ Homework/Homework_08/Ex062/Program.cs
@@ -0,0 +1,13 @@
+﻿using static MethodsHelix;
+
+Console.WriteLine("Задача 62, заполнить массив 4х4 по спиралью числами от 1 до 16");
+
+int rowP = 4;
+int colP = 4;
+int count = 1;
+
+int[,] array = new int[rowP, colP];
+
+array = FillArrowHelix(rowP, colP, count, array);
+
+PrintArray(array);
