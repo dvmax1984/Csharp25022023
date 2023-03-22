106904b41c5d05b2797b2eea289f5c2d4012b73c
Maksim Dvortsov <55104764+dvmax1984@users.noreply.github.com>
2023-03-22 11:54:03 +0000

    7

--- Lecture/Lecture_07/Ex000_Fill_Rec/Program.cs
+++ Lecture/Lecture_07/Ex000_Fill_Rec/Program.cs
@@ -1,35 +1,45 @@
-﻿int size = 4; //Convert.ToInt32(Console.ReadLine());
+
+
+int size = 4; //Convert.ToInt32(Console.ReadLine());
 
 int[,] pic = new int[size, size];
 
 int limX = pic.GetLength(0)-1;
 int limY = pic.GetLength(1)-1;
 
-// for (int i = 0; i < 4; i++)
-// {
-//     pic[0, i] = 0;
-//     pic[i, 0] = 0;
-//     pic[i, limY] = 0;
-//     pic[limX, i] = 0;
-// }
+for (int i = 0; i < limX; i++)
+{
+     pic[0, i] = 0;
+     pic[i, 0] = 0;
+     pic[i, limY] = 0;
+     pic[limX, i] = 0;
+}
 
-//PrintImage(pic);
-FillImage(0, 0, 1, 6);
+
+          
+
+
+
+PrintImage(pic);
+FillImage(0, 0, 1, 0);
 Console.WriteLine();
 PrintImage(pic);
 
 void FillImage(int row, int col, int count, int dir)
 {
-    if (pic[row, col] == 0 )
-    {
+    if (pic[row, col] ==0)
+   {
         pic[row, col] = count;
-        if (col < limX && (dir !=4 || dir !=8))  FillImage(row, col+1, count+=1, 0);    //  > 6
-        if (row < limY && (dir !=4 || dir !=8))  FillImage(row+1, col, count+=1, 5);    //  v 5
-        if (col !=0)                FillImage(row, col-1, count+=1, 4);    //  < 4
 
-        if (col == 0 && row > 0 )  FillImage(row-1, col, count+=1, 8);    //  ^ 8        
+        if (col < limX && pic[row, col+1] ==0 && dir !=8)   FillImage(row, col+1, count+=1, 0);    //  > 6 Ok!
+
+        if (row >= 0 && row != limY)    FillImage(row+1, col, count+=1, 0);    //  v 5 Ok!  || pic[row+1, col] == 0 && dir !=8)
+
+        if (row == limY-1 && pic[row, col-1] == 0 )           FillImage(row, col-1, count+=1, 0);    //  < 4
+        //if (row == limY || pic[row+1, col] == 0 )           FillImage(row, col-1, count+=1, 0);    //  < 4
+
+        //if (col ==0 || pic[row, col-1] == 0 )               FillImage(row-1, col, count+=1,1);    //  > 6       
     }
-    if (row > 1  && (dir == 8 && pic[row-1, col] != 0))   FillImage(row, col+1, count+=1, 6);    //  > 6
 }
 
 void PrintImage(int[,] image)
@@ -43,5 +53,4 @@ void PrintImage(int[,] image)
         }
         Console.WriteLine();
     }
-
 }
