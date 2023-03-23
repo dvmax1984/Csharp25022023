4cfdf090c70f0495c84990e1695ff67064596ee9
Maxim Dvortcov <dvmax@outlook.com>
2023-03-23 10:20:20 +0700

    Правка кода в задаче 36 ДЗ 8

--- Homework/Homework_08/Ex060/Program.cs
+++ Homework/Homework_08/Ex060/Program.cs
@@ -8,26 +8,26 @@
         27(0,0,1) 90(0,1,1)
         26(1,0,1) 55(1,1,1)*/
 
-using static class clMethod;
+using static clMethod;
 
 Console.WriteLine("Hello, World!");
 
-int[,,] array = new int[2,2,2];  //row - r, col - c, block - b
+int[,,] array = new int[2, 2, 2];  //row - r, col - c, block - b
 
 
-for (int b = 0; b < array.GetLegth(3); b++)
-  {
-  for (int r = 0; r < array.GetLegth(0), r++)
+for (int b = 0; b < array.GetLength(3); b++)
+{
+    for (int r = 0; r < array.GetLength(0); r++)
     {
-      for (int c = 0; c < array.GetLegth(1), c++)
+        for (int c = 0; c < array.GetLength(1); c++)
         {
-          int value2check = new rnd.Next(10,99);
-          if (CheckIsNotValueInArray(value2check, array)) 
-          {
-            array[r,c,b] = value2check;
-          }
+            int value2check = new Random().Next(10, 99);
+            if (CheckIsValueInArray(value2check, array))
+            {
+                array[r, c, b] = value2check;
+            }
         }
     }
-  }
+}
 
 Console.Write(PrintArray3D(array));
--- Homework/Homework_08/Ex060/clMethods.cs
+++ Homework/Homework_08/Ex060/clMethods.cs
@@ -1,41 +1,38 @@
 public class clMethod
 {
-
-public static bool CheckIsNotValueInArray(int checkValue, int[,,] array)
-{
-bool result = true;
-
-for (int b = 0; b < array.GetLegth(3); b++)
-  {
-  for (int r = 0; r < array.GetLegth(0), r++)
+    public static bool CheckIsValueInArray(int checkValue, int[,,] array)
     {
-      for (int c = 0; c < array.GetLegth(1), c++)
-        {
-          // если найдено проверяемое значение, то вернуть FALSE и выйти из циклов
-          if (checkValue == array[r,c,b]) {return result = false; break;}  
-        }
-    }
-  }
-return result;
-}
+        bool result = true;
 
-public static string PrintArray3D(int[,,] array)
-    {
-      string result = String.Empty;
-        
-      for (int b = 0; b << array.GetLength(3); b++)
-      {
-        for (int r = 0; r < array.GetLength(0); r++)
+        for (int b = 0; b < array.GetLength(3); b++)
         {
-            for (int c = 0; c < array.GetLength(1); c++)
+            for (int r = 0; r < array.GetLength(0); r++)
             {
-                result += $"{array[r, c, b]} ";
+                for (int c = 0; c < array.GetLength(1); c++)
+                {
+                    // если найдено проверяемое значение, то вернуть FALSE и выйти из циклов
+                    if (checkValue == array[r, c, b]) { return result = false; }
+                }
             }
-            result += $"\n";
         }
-      } 
-      return result += $"\n";
+        return result;
     }
 
-  
+    public static string PrintArray3D(int[,,] array)
+    {
+        string result = String.Empty;
+
+        for (int b = 0; b < array.GetLength(3); b++)
+        {
+            for (int r = 0; r < array.GetLength(0); r++)
+            {
+                for (int c = 0; c < array.GetLength(1); c++)
+                {
+                    result += $"{array[r, c, b]} ";
+                }
+                result += $"\n";
+            }
+        }
+        return result += $"\n";
+    }
 }
