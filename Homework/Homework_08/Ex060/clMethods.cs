373819ce90663960ff72a4091513e9e7b916a902
Maksim Dvortsov <55104764+dvmax1984@users.noreply.github.com>
2023-03-23 05:18:27 +0000

    Продолжаю Решение задачи 60 ДЗ 8

--- Homework/Homework_08/Ex060/clMethods.cs
+++ Homework/Homework_08/Ex060/clMethods.cs
@@ -1,11 +1,5 @@
 public static class MethodCheck
 {
-  //public static int[,,] GetUniqValue(int value2check)
-  //{
-  //  while ()
-  //}
-  
-  
   public static int[,,] FillArrayUniqValue(int[,,] array) 
 {
 for (int b = 0; b < array.GetLength(2); b++)
@@ -14,38 +8,21 @@ for (int b = 0; b < array.GetLength(2); b++)
     {
         for (int c = 0; c < array.GetLength(1); c++)
         {
-         int value2check = 0;// new Random().Next(10, 18);
+         //int value2check = 0;// new Random().Next(10, 18);
 
-bool isUnique;
+                bool isUnique;
                 do
                 {
-                    array[r,c,b] = random.Next(10, 20);
+                    array[r,c,b] = new Random().Next(10, 99);
                     isUnique = true;
                     
-                  for (int j = 0; j < i; ++j)
-
-                    if (!CheckIsValueInArray(array[r,c,b], array))                  
-                    //if (array[r,c,b] == array[r,c,b])
+                    // если найдено проверяемое значение, то оно не уникально
+                    if (CheckIsValueInArray(array[r,c,b], array))                  
                         {
                             isUnique = false;
                             break;
                         }
                 } while (!isUnique);
-          
-          
-          
-          //if (!CheckIsValueInArray(value2check, array))
-            //{
-            //    array[r, c, b] = value2check;
-            //}
-          //else
-          //{
-           // value2check = new Random().Next(10, 18);
-           // }
-            //while (!CheckIsValueInArray(value2check, array))
-            //{
-            //  array[r, c, b] = value2check;
-            //}
         }
     }
 }
@@ -61,22 +38,23 @@ bool isUnique;
                 for (int c = 0; c < array.GetLength(1); c++)
                 {
                     // если найдено проверяемое значение, то вернуть TRUE и выйти из циклов
-                    if (checkValue == array[r, c, b]) { return true; }
+                    if (checkValue == array[r, c, b])
+                  {
+                    Console.WriteLine($"есть такое {checkValue} в ячейке: {r}, {c}, {b}");
+                    return true; 
+                    }
+                  else 
+                  { 
+                    Console.WriteLine($"нет такого");
+                    return false; 
+                    }
+                  
                 }
             }
         }
         return false;
     }
 
-// TARGER
-//Напишите программу, которая будет построчно выводить массив, 
-//добавляя индексы каждого элемента.
-//Массив размером 2 x 2 x 2
-/*      66(0,0,0) 25(0,1,0)
-        34(1,0,0) 41(1,1,0)
-        27(0,0,1) 90(0,1,1)
-        26(1,0,1) 55(1,1,1)    */
-  
     public static string PrintArray3D(int[,,] array)
     {
         string result = String.Empty;
@@ -87,13 +65,10 @@ bool isUnique;
             {
                 for (int c = 0; c < array.GetLength(1); c++)
                 {
-                  //Console.Write($" {array[r, c, b]}({r},{c},{b})");
                   result += $" {array[r, c, b]}({r},{c},{b})";
                 }
-            //Console.WriteLine($" ");//" row: {r}");
             result += $"\n";  
             }
-          //Console.WriteLine($" ");//block: {b}");
           result += $"\n";
         }
         return result;
