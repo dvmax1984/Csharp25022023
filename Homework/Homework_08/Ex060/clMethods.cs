91ada3ee527f50c641dc427f7dec739e12c20603
Maksim Dvortsov <55104764+dvmax1984@users.noreply.github.com>
2023-03-23 03:12:50 +0000

    Решаю задачу 60 ДЗ 8

---
+++ Homework/Homework_08/Ex060/clMethods.cs
@@ -0,0 +1,41 @@
+public class clMethod
+{
+
+public static bool CheckIsNotValueInArray(int checkValue, int[,,] array)
+{
+bool result = true;
+
+for (int b = 0; b < array.GetLegth(3); b++)
+  {
+  for (int r = 0; r < array.GetLegth(0), r++)
+    {
+      for (int c = 0; c < array.GetLegth(1), c++)
+        {
+          // если найдено проверяемое значение, то вернуть FALSE и выйти из циклов
+          if (checkValue == array[r,c,b]) {return result = false; break;}  
+        }
+    }
+  }
+return result;
+}
+
+public static string PrintArray3D(int[,,] array)
+    {
+      string result = String.Empty;
+        
+      for (int b = 0; b << array.GetLength(3); b++)
+      {
+        for (int r = 0; r < array.GetLength(0); r++)
+        {
+            for (int c = 0; c < array.GetLength(1); c++)
+            {
+                result += $"{array[r, c, b]} ";
+            }
+            result += $"\n";
+        }
+      } 
+      return result += $"\n";
+    }
+
+  
+}
